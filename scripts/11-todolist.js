836c6289fd0711f45c4fbaeeb3e4a510292c3305
ranjan-uprety <ranjanuprety01@gmail.com>
2025-08-13 23:19:41 +0545

    updated todolist to show the list of todos in the page itself

--- scripts/11-todolist.js
+++ scripts/11-todolist.js
@@ -1,5 +1,34 @@
 const todoList = [];
 
+renderTodoList();
+
+function renderTodoList(){
+
+  let todolistHTML = '';
+
+  for (let i = 0; i < todoList.length; i++){
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todolistHTML += html;
+
+  };
+
+  console.log(todolistHTML);
+
+  document.querySelector('.js-todo-list').innerHTML = todolistHTML;
+  }
+
 function addTodo(){
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+  
+  todoList.push(name);
+  console.log(todoList);
+
+  inputElement.value = '';
+
+
+  renderTodoList();
+
   
 }
