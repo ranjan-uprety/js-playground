bc71b8bd087afef5513ca8705c5da2d2cfa6e8f7
ranjan-uprety <ranjanuprety01@gmail.com>
2025-08-15 23:09:20 +0545

    updated todolist with event listeners

--- scripts/12-todolist.js
+++ scripts/12-todolist.js
@@ -19,10 +19,7 @@ function renderTodoList(){
     const html = `
     <div>${name}</div>
     <div>${dueDate}</div>
-    <button onclick="
-    todoList.splice(${index}, 1);
-    renderTodoList();
-    " class = "delete-todo-button">Delete</button>
+    <button class = "delete-todo-button js-delete-todo-button">Delete</button>
     
   `;
     todolistHTML += html;
@@ -31,7 +28,23 @@ function renderTodoList(){
   });
   
   document.querySelector('.js-todo-list').innerHTML = todolistHTML;
+  
+
+  document.querySelectorAll('.js-delete-todo-button')
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener('click', () => {
+        todoList.splice(index, 1);
+        renderTodoList();
+        
+      });
+      
+    });
+
   }
+
+  document.querySelector('.js-add-todo-button').addEventListener('click', () => {
+    addTodo();
+  })
 
 function addTodo(){
   const inputElement = document.querySelector('.js-name-input');
